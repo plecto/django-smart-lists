9b0aae9f98f9001855671e85a8946cd65e1314b0
Kristian Øllegaard <kristian@kristian.io>
2017-03-11 21:55:15 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-smart-lists',
+      version='1.0',
+      description='Easy lists for django views',
+      author='Kristian Oellegaard',
+      author_email='kristian@kristian.io',
+      url='https://github.com/plecto/django-smart-lists',
+      packages=['smart_lists'],
+)
