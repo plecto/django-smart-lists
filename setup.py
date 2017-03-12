93b1e1f8900a0432a4a73d7aa8214aa07f231374
Kristian Øllegaard <kristian@kristian.io>
2017-03-12 18:40:48 +0100

    New release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='django-smart-lists',
-      version='1.0.8',
+      version='1.0.10',
       description='Easy lists for django views',
       author='Kristian Oellegaard',
       author_email='kristian@kristian.io',
