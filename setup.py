936873c95641593237ace37a66f9899ad831eb40
Kristian Øllegaard <kristian@kristian.io>
2017-03-12 10:26:29 +0100

    Fixed this packaging mess

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@
 from distutils.core import setup
 
 setup(name='django-smart-lists',
-      version='1.0.1',
+      version='1.0.8',
       description='Easy lists for django views',
       author='Kristian Oellegaard',
       author_email='kristian@kristian.io',
       url='https://github.com/plecto/django-smart-lists',
+      include_package_data=True,
       packages=['smart_lists', 'smart_lists.templatetags'],
+      package_data={'smart_lists' : ['templates/smart_lists/*']},
 )
