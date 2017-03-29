49caaaf8289ea171d6f593a0fb1251637ed4f74c
Kristian Øllegaard <kristian@kristian.io>
2017-03-29 23:23:05 +0200

    improved setup.py and new version

--- setup.py
+++ setup.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python
+import os
+from setuptools import find_packages, setup
 
-from distutils.core import setup
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
 
-setup(name='django-smart-lists',
-      version='1.0.14',
-      description='Easy lists for django views',
-      author='Kristian Oellegaard',
-      author_email='kristian@kristian.io',
-      url='https://github.com/plecto/django-smart-lists',
-      include_package_data=True,
-      packages=['smart_lists', 'smart_lists.templatetags'],
-      package_data={'smart_lists' : ['templates/smart_lists/*']},
-      install_requires=['Django']
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-smart-lists',
+    version='1.0.15',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Easy lists for django views',
+    long_description=README,
+    url='https://github.com/plecto/django-smart-lists',
+    author='Kristian Oellegaard',
+    author_email='kristian@kristian.io',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
 )
