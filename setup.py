c52418cc7f8af81840f4eea461724a1f3c97a160
Kristian Øllegaard <kristian@kristian.io>
2018-09-30 16:35:16 +0200

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-smart-lists',
-    version='1.0.21',
+    version='1.0.22',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
